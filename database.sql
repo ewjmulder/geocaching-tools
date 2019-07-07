e49bcde86291b3e9f270d4686d7139ec6206eb2c
Erik Mulder <ewjmulder@yahoo.com>
2019-07-07 23:15:26 +0200

    Gradle + Kotlin setup PoC

--- database.sql
+++ database.sql
@@ -8,6 +8,7 @@ DROP TYPE GC_SIZE;
 
 CREATE TYPE GC_TYPE AS ENUM ('traditional', 'multi', 'mystery', 'letterbox', 'earth', 'virtual', 'event', 'webcam', 'cito-event', 'wherigo', 'reverse', 'mega-event', 'giga-event');
 CREATE TYPE GC_SIZE AS ENUM ('micro', 'small', 'regular', 'large', 'other', 'virtual', 'unknown');
+CREATE TYPE GC_STATUS AS ENUM ('enabled', 'disabled');
 
 CREATE TABLE caches (
   id INT PRIMARY KEY,
@@ -15,6 +16,7 @@ CREATE TABLE caches (
   name VARCHAR(1024),
   type GC_TYPE,
   location GEOGRAPHY(POINT, 4326),
+  status GC_STATUS,
   size GC_SIZE,
   difficulty NUMERIC(2, 1) CHECK (difficulty = 1.0 OR difficulty = 1.5 OR difficulty = 2.0 OR difficulty = 2.5 OR difficulty = 3.0 OR difficulty = 3.5 OR difficulty = 4.0 OR difficulty = 4.5 OR difficulty = 5.0),
   terrain NUMERIC(2, 1) CHECK (terrain = 1.0 OR terrain = 1.5 OR terrain = 2.0 OR terrain = 2.5 OR terrain = 3.0 OR terrain = 3.5 OR terrain = 4.0 OR terrain = 4.5 OR terrain = 5.0),
@@ -37,7 +39,7 @@ CREATE TABLE dnfs (
   cache_id INT REFERENCES caches(id)
 );
 
-INSERT INTO caches VALUES (1, 'GC81X1W', 'Beukenstein - De Heuvel / The Hill', 'traditional', 'SRID=4326;POINT(5.2763 52.059333)', 'small', 1.5, 2.0, '2018-12-30', 'Team_Spoorloos');
+INSERT INTO caches VALUES (1, 'GC81X1W', 'Beukenstein - De Heuvel / The Hill', 'traditional', 'SRID=4326;POINT(5.2763 52.059333)', 'enabled', 'small', 1.5, 2.0, '2018-12-30', 'Team_Spoorloos');
 INSERT INTO users VALUES (1, 'Team_Spoorloos');
 INSERT INTO finds VALUES (1, 1);
 INSERT INTO dnfs VALUES (1, 1);
---
+++ src/main/kotlin/tryout/Tryout.kt
@@ -0,0 +1,34 @@
+package tryout
+
+import java.io.File
+import java.util.concurrent.TimeUnit
+import kotlinx.serialization.*
+import kotlinx.serialization.json.*
+
+@Serializable
+data class CacheResults(val results: List<CacheResult>)
+@Serializable
+data class CacheResult(val id: Int)
+
+
+fun main(args: Array<String>) {
+    println("Start gathering caches...")
+    val json = "./get_caches.sh 52 5 51 6 0 5".runCommand()
+    println("result: " + parse(json))
+}
+
+fun parse(jsonAsString: String) : CacheResults {
+    return Json.nonstrict.parse(CacheResults.serializer(), jsonAsString)
+}
+
+fun String.runCommand(workingDir: File = File("."),
+                      timeoutAmount: Long = 60,
+                      timeoutUnit: TimeUnit = TimeUnit.SECONDS): String =
+    ProcessBuilder(*this.split("\\s".toRegex()).toTypedArray())
+        .directory(workingDir)
+        .redirectOutput(ProcessBuilder.Redirect.PIPE)
+        .redirectError(ProcessBuilder.Redirect.PIPE)
+        .start().apply {
+          waitFor(timeoutAmount, timeoutUnit)
+        }.inputStream.bufferedReader().readText()
+
